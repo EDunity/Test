d97a255a200680ecc5cc64e91712db11d561e5d3
EDunity <50490026+EDunity@users.noreply.github.com>
2021-01-09 14:29:26 +0900

    ada

--- cgi-bin/sample.py
+++ cgi-bin/sample.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+#import subprocess
+import cgi
+import cgitb
+#import pyautogui
+#import time
+
+cgitb.enable()
+
+print("Content-Type: text/html")
+print()   
+
+form = cgi.FieldStorage()
+
+#time.sleep(3)
+#pyautogui.typewrite(form["Sentence"].value, 0.25)
+print(form["Sentence"].value)
